718b55085fbfe70b8a03d597e2fd93b4f4b4166a
ankit4a4 <singhankit23038@gmail.com>
2025-11-03 17:25:02 +0530

    letest code push

--- components/Gallery/GallerySection.jsx
+++ components/Gallery/GallerySection.jsx
@@ -54,10 +54,9 @@ import img50 from "../../public/Images/gallery/26.jpg";
 
 const galleryImages = [
   img1, img2, img3, img4, img5, img6, img7, img8, img9, img10,
-  img11, img12, img13, img14, img15, img16, img17, img18, img19, img20,
-  img21, img22, img23, img24, img25, img26, img27, img28, img29, img30,
-  img31, img32, img33, img34, img35, img36, img37, img38, img39, img40,
-  img41, img42, img43, img44, img45, img46, img47, img48, img49, img50,
+  img11, img12, img13, img14, img15, img16,  img18, img19, img20,
+    img23,   img26, img27, img28, 
+  img31,   img34, img35, img36,  img38, img39,  img42, img43, img44, img45, img46, img47, img48, 
 ];
 
 export default function GallerySection() {
@@ -67,17 +66,17 @@ export default function GallerySection() {
         <h2 className="text-4xl font-bold font-alegreya italic text-center mb-16 text-yellow-700/90">
           Our Gallery
         </h2>
-        <div className="columns-1 sm:columns-2 lg:columns-3 gap-4 space-y-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 space-y-4">
           <AnimatePresence>
             {galleryImages.map((src, i) => (
               <div
                 key={i}
-                className="relative overflow-hidden rounded-xl shadow-lg hover:shadow-xl transition-all duration-300 break-inside-avoid"
+                className="relative h-[300px]  overflow-hidden rounded-xl shadow-lg hover:shadow-xl transition-all duration-300 break-inside-avoid"
               >
                 <Image
                   src={src}
-                  alt={`Gallery image ${i + 1}`}
-                  className="w-full h-auto rounded-xl object-contain transition-transform duration-500 hover:scale-[1.03]"
+                  alt={`Gallery image}`}
+                  className="w-full h-[100%] rounded-xl object-cover transition-transform duration-500 hover:scale-[1.03]"
                   placeholder="blur"
                 />
               </div>
